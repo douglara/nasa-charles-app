834509826d1da585f16082b0ae61b48ed8c86cda
Douglas Lara <douglasdelaras@gmail.com>
2019-10-28 20:07:21 -0300

    removed unit test

--- test/controllers/whatsapp_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WhatsappControllerTest < ActionDispatch::IntegrationTest
-  test "should get webhook" do
-    get whatsapp_webhook_url
-    assert_response :success
-  end
-
-end
